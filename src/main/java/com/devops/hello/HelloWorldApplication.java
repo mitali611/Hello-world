a476c30e4029e00c97d5ec172b7b5794bb915f35
mitali611 <51718432+mitali611@users.noreply.github.com>
2021-01-05 15:23:23 +0530

    Add files via upload

---
+++ src/main/java/com/devops/hello/HelloWorldApplication.java
@@ -0,0 +1,24 @@
+package com.devops.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+@SpringBootApplication
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+    String home() {
+        return "Hello World - v3!";
+    }
+	
+
+}
